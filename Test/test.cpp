27f56a3a4cacb536d45ba31856668137cce5e153
ravenxrz <zhang.xingrui@foxmail.com>
2020-10-22 09:48:25 +0800

    Leetcode 17

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,74 +1,55 @@
 /*
- * @lc app=leetcode.cn id=77 lang=cpp
+ * @lc app=leetcode.cn id=17 lang=cpp
  *
- * [77] 组合
+ * [17] 电话号码的字母组合
  */
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
+
+
 
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> ans;
+    vector<string> ans;
 
-    void dfs1(int idx, int n, int k, vector<int> tmp){
-        if(tmp.size() == k){
-            ans.push_back(tmp);
-            return;
-        }
+    vector<string> strmapping = {
+        "",         // 占位符
+        "",         // 占位符
+        "abc",      //2
+        "def",      //3
+        "ghi",      //4
+        "jkl",      //5
+        "mno",      //6
+        "pqrs",     //7
+        "tuv",      //8
+        "wxyz"      //9
+    };
 
-        if(idx > n){
-            return;
-        }
+    void dfs(string digits,  string one, int didx, int oidx){
+            if(oidx == one.size()){
+                ans.push_back(one);
+                return ;
+            }
 
-        // put
-        tmp.push_back(idx);
-        dfs1(idx + 1, n, k, tmp);
-        // remove
-        tmp.pop_back();
-        dfs1(idx + 1, n, k, tmp);
+            int map_idx = digits[didx] - '0';
+            for(int i = 0;i<strmapping[map_idx].size();i++){
+                // put
+                one[oidx] = strmapping[map_idx].at(i);
+                dfs(digits,one,didx+1,oidx+1);
+                // remove
+                dfs(digits,one,didx+1,oidx);
+            }
     }
 
-    vector<int> tset;
-    void init_set(int  n){
-        for (int i = 1; i <= n;i++){
-            tset.push_back(i);
-        }
-    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return ans;
 
-    void dfs2(int idx, int n, int k, vector<int> tmp){
-        if(tmp.size() == k){
-            vector<int> ret;
-            ret.resize(tset.size() - k);
-            set_difference(tset.begin(),tset.end(),tmp.begin(),tmp.end(),ret.begin());
-            ans.push_back(ret);
-            return;
-        }
-
-        if(idx > n){
-            return;
-        }
-
-        // put
-        tmp.push_back(idx);
-        dfs2(idx + 1, n, k, tmp);
-        // remove
-        tmp.pop_back();
-        dfs2(idx + 1, n, k, tmp);
-    }
-
-
-    vector<vector<int>> combine(int n, int k)
-    {
-        if(k < n/2){
-            dfs1(1, n, k, {});
-        }else{
-            init_set(n);
-            dfs2(1, n, n-k, {});
-        }
-
+        const string& one(digits);
+        dfs(digits, one,0,0 );
         return ans;
     }
 };
@@ -77,7 +58,9 @@ public:
 int main()
 {
     Solution sol;
-    sol.combine(4,2);
-
+    sol.letterCombinations("23");
+    for(const string &str : sol.ans){
+        cout << str << endl;
+    }
     return 0;
 }
