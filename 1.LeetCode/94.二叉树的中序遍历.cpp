8273959c33882e74d912c2f5ec89c6e24bb4b2c0
Raven <zhang.xingrui@foxmail.com>
2020-11-07 16:05:38 +0800

    leetcode 94

--- 1.LeetCode/94.二叉树的中序遍历.cpp
+++ 1.LeetCode/94.二叉树的中序遍历.cpp
@@ -4,20 +4,6 @@
  * [94] 二叉树的中序遍历
  */
 
-#include <vector>
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-
-
 // @lc code=start
 /**
  * Definition for a binary tree node.
