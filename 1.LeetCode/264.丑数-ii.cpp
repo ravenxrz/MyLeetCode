ab235b8fcecbafa57e8f5b63645922c382989fb2
ravenxrz <zhang.xingrui@foxmail.com>
2020-11-09 10:20:12 +0800

    leetcode 264

--- 1.LeetCode/264.丑数-ii.cpp
+++ 1.LeetCode/264.丑数-ii.cpp
@@ -15,37 +15,31 @@ public:
         vector<int> uglies { 1, 2, 3,4, 5 };
         if(n <= uglies.size())
             return uglies[n - 1];
-        int i2 = 1;
-        int i3 = 0;
-        int i5 = 0;
+        int i2 = 1;		// uglies[i2] = uglies[1] = 2 ==> 代表当前走到了2x2
+        int i3 = 0;		// ==> 1x3
+        int i5 = 0;		// ==> 1x5
         int v2, v3, v5;
         int counter = 5;
-        int max = 5;
+        int min_val = 5;
         while(counter != n){
             v2 = uglies[i2 + 1] * 2;
             v3 = uglies[i3 + 1] * 3;
             v5 = uglies[i5 + 1] * 5;
-            max = min({ v2, v3, v5 });
-            uglies.push_back(max);
-            if(max == v2){
+            min_val = min({ v2, v3, v5 });
+            uglies.push_back(min_val);
+            if(min_val == v2){
                 i2++;
             }
-            if(max == v3){
+            if(min_val == v3){
                 i3++;
             }
-            if (max == v5){
+            if (min_val == v5){
                 i5++;
             }
             counter++;
         }
-        return max;
+        return min_val;
     }
 };
 // @lc code=end
 
-int main()
-{
-    Solution sol;
-    cout << sol.nthUglyNumber(11);
-    return 0;
-}
