d4ad5848633107f144136537d652eb4eaa9770ed
ravenxrz <zhang.xingrui@foxmail.com>
2020-11-19 10:45:37 +0800

    0.数据结构-8.内部排序-冒泡排序

---
+++ 0.DataStructure/8.InternalSorting/bubble_sort.cpp
@@ -0,0 +1,41 @@
+//
+// Bubble sort
+//
+
+#include "common.h"
+
+class BubbleSorter : public Sorter{
+    /**
+     * 冒泡排序 复杂度O(n^2)
+     * 10k数据量 0.738 s
+     * 100k数据量 77.5 s
+     * @param nums
+     */
+    void _sort(std::vector<int> &nums) override{
+        int tmp;
+        for(int i = 0; i<nums.size()-1;i++){
+            bool swapped = false;
+            for(int j = 0;j<nums.size()-1-i;j++){
+                if(nums[j] > nums[j+1]){
+                    tmp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = tmp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){   /* 提前终止 */
+                break;
+            }
+        }
+    }
+};
+
+int main() {
+    std::vector<int> nums = generateRandomNumbers(8, 1, 10);
+    printNums(nums);
+    Sorter *sorter = new BubbleSorter();
+    sorter->sort(nums);
+//    printNums(nums);
+    delete sorter;
+    return 0;
+}
