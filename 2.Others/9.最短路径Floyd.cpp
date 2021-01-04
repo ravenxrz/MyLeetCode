d1dc7d2b8b7b4b7bfa16c5ac2f1a28fd842f9e21
ravenxrz <zhang.xingrui@foxmail.com>
2021-01-04 19:43:26 +0800

    动态规划-warshall&floyd

---
+++ 2.Others/9.最短路径Floyd.cpp
@@ -0,0 +1,12 @@
+//
+// Created by a2855 on 2021/1/4.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
--- 3.Algorithm/8.传递闭包.cpp
+++ 3.Algorithm/8.传递闭包.cpp
@@ -1,39 +1,33 @@
-//
-// Created by a2855 on 2021/1/4.
-//
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-#define INF 100000
-
 int main()
 {
-    vector<vector<int>> graph = {
-            {0, INF, 3,   INF},
-            {2,   0, INF, INF},
-            {INF, 7, 0,   1},
-            {6, INF, INF, 0},
+    vector<vector<bool>> graph{
+            {0, 1, 0, 0},
+            {0, 0, 0, 1},
+            {0, 0, 0, 0},
+            {1, 0, 1, 0}
     };
     
-    // floyd
+    // warshall
     for (int k = 0; k < graph.size(); k++) {
         for (int i = 0; i < graph.size(); i++) {
             for (int j = 0; j < graph.size(); j++) {
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+                graph[i][j] = (graph[i][k] & graph[k][j]) | graph[i][j];
             }
         }
     }
+    
     // print
     for (int i = 0; i < graph.size(); i++) {
         for (int j = 0; j < graph.size(); j++) {
-           cout << graph[i][j] << " ";
+            cout << graph[i][j] << " ";
         }
         cout << endl;
     }
-    
-    
+    cout << endl;
     return 0;
 }
-
