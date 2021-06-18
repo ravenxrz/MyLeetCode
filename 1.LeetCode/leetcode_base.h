3b04535ce59066f419a2bb13d328115c358eaf04
raven <zhang.xingrui@foxmial.com>
2021-06-18 09:58:32 +0800

    lc 238

--- 1.LeetCode/leetcode_base.h
+++ 1.LeetCode/leetcode_base.h
@@ -6,60 +6,82 @@
 #ifndef TEST_FOR_MYLEETCODE_LEETCODE_BASE_H
 #define TEST_FOR_MYLEETCODE_LEETCODE_BASE_H
 
-#include <bits/stdc++.h>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
 using namespace std;
 
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
-void createTree(TreeNode **node) {
-  int x;
-  cin >> x;
-  if (x == -1) {
-    *node = nullptr;
-    return;
-  }
-  *node = new TreeNode(x);
-  createTree(&(*node)->left);
-  createTree(&(*node)->right);
-}
-
-ListNode *createList() {
-  ListNode *head = nullptr;
-  ListNode *tail = nullptr;
-  int x;
-  while (cin >> x) {
-    if(x == -1) break;
-    if (head == nullptr) {
-      head = new ListNode(x);
-      tail = head;
-    } else {
-      ListNode *node = new ListNode(x);
-      tail->next = node;
-      tail = node;
+void createTree(TreeNode **node)
+{
+    int x;
+    cin >> x;
+    if (x == -1)
+    {
+        *node = nullptr;
+        return;
     }
-  }
-  return head;
+    *node = new TreeNode(x);
+    createTree(&(*node)->left);
+    createTree(&(*node)->right);
 }
 
-void printList(ListNode *head){
-  while(head){
-    cout << head->val << " ";
-    head = head->next;
-  }
+ListNode *createList()
+{
+    ListNode *head = nullptr;
+    ListNode *tail = nullptr;
+    int x;
+    while (cin >> x)
+    {
+        if (x == -1)
+            break;
+        if (head == nullptr)
+        {
+            head = new ListNode(x);
+            tail = head;
+        }
+        else
+        {
+            ListNode *node = new ListNode(x);
+            tail->next = node;
+            tail = node;
+        }
+    }
+    return head;
+}
+
+void printList(ListNode *head)
+{
+    while (head)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
 }
 
 #endif // TEST_FOR_MYLEETCODE_LEETCODE_BASE_H
