df9633546112c3e53855b85510160a38b6e8a8c9
ravenxrz <zhang.xingrui@foxmail.com>
2021-08-09 19:05:15 +0800

    CI 68-1 68-2

---
+++ 1.LeetCode/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,25 @@
+#include "leetcode_base.h"
+
+
+class Solution {
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return LCA(root, p, q);
+    }
+
+   private:
+    TreeNode* LCA(TreeNode* cur, TreeNode* p, TreeNode* q) {
+        if (cur == p || cur == q || cur == nullptr) {
+            return cur;
+        }
+        TreeNode* left = LCA(cur->left, p, q);
+        TreeNode* right = LCA(cur->right, p, q);
+        if (left && right) {
+            return cur;
+        } else if (left) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+};
